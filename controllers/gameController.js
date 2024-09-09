80e425ab966f9d7c1bb8708a45a66d50618b1a73
Neil Taison Rigaud <ntsrigaud14@gmail.com>
2024-09-09 12:15:28 +0800

    fix: refactor template add game form to also edit and add api edit route for game info

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -2,7 +2,12 @@
 const multer = require('multer');
 const { body, validationResult } = require('express-validator');
 
-const { getAllGames, addGame } = require('../models/game');
+const { 
+  getAllGames, 
+  getGameById,
+  updateGameById,
+  addGame
+} = require('../models/game');
 const { addImageToBucket, getImageSignedUrl } = require('../lib/awsUtils');
 
 const storage = multer.memoryStorage();
@@ -27,8 +32,7 @@ const renderIndex = async (req, res) => {
 // Render the page for adding a new game
 const addGameGet = async (req, res) => {
   try {
-    const games = await getAllGames();
-    res.render('add_game', { games });
+    res.render('form_game');
   } catch (error) {
     console.error('Error fetching games for add game page:', error);
     res.status(500).send('Internal Server Error');
@@ -67,9 +71,55 @@ const addGamePost = [
   }
 ];
 
+const editGameGet = async (req, res) => {
+  try {
+    const game = await getGameById(req.params.id);
+    if (!game) {
+      return res.status(404).send('Game not found');
+    }
+
+    res.render('form_game', { game });
+  } catch (error) {
+    console.error('Error fetching game for edit page:', error);
+    res.status(500).send('Internal Server Error');
+  }
+}
+
+const editGamePost = [
+  // Validate and sanitize input
+  upload.single('image'),
+  body('title').isString().trim().notEmpty().withMessage('Title is required'),
+  body('description').isString().trim().notEmpty().withMessage('Description is required'),
+  body('genre').isString().trim().notEmpty().withMessage('Genre is required'),
+  body('release_date').isDate().withMessage('Release date must be a valid date'),
+  
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).render('edit_game', { 
+        errors: errors.array() 
+      });
+    }
+
+    try {
+      const game = req.body;
+
+      if (req.file) game.image_key = await addImageToBucket(req.file.buffer, req.file.mimetype, req.file.originalname);
+
+      await updateGameById(req.params.id, game);
+      res.redirect('/');
+    } catch (error) {
+      console.error('Error updating game:', error);
+      res.status(500).send('Internal Server Error');
+    }
+  }
+];
+
 module.exports = { 
   renderIndex, 
   addGameGet,
   addGamePost,
+  editGameGet,
+  editGamePost
 };
 
--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,7 @@ const game_controller = require('../controllers/gameController');
 router.get('/', game_controller.renderIndex);
 router.get('/add-game', game_controller.addGameGet);
 router.post('/add-game', game_controller.addGamePost);
+router.get('/edit-game/:id', game_controller.editGameGet);
+router.post('/edit-game/:id', game_controller.editGamePost);
 
 module.exports = router;
