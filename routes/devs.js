e46e2c179906bbfdcace3636a678ad42859e574f
Neil Taison Rigaud <ntsrigaud14@gmail.com>
2024-09-10 19:15:19 +0800

    add: admin authentication for devs api route

--- routes/devs.js
+++ routes/devs.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const checkAdmin = require('../middleware/checkAdmin');
 const authenticateJWT = require('../middleware/auth');
 
 const dev_controller = require('../controllers/devController');
 
 // Route to render the index page
-router.get('/', authenticateJWT ,dev_controller.renderIndex);
-router.get('/add', dev_controller.addDeveloperGet);
-router.post('/add', dev_controller.addDeveloperPost);
-router.get('/:id/edit', dev_controller.editDeveloperGet);
-router.post('/:id/edit', dev_controller.editDeveloperPost);
-router.get('/:id/delete', dev_controller.deleteDeveloperGet);
-router.post('/:id/delete', dev_controller.deleteDeveloperPost);
+router.get('/', authenticateJWT, checkAdmin, dev_controller.renderIndex);
+router.get('/add', authenticateJWT, checkAdmin, dev_controller.addDeveloperGet);
+router.post('/add', authenticateJWT, checkAdmin, dev_controller.addDeveloperPost);
+router.get('/:id/edit', authenticateJWT, checkAdmin, dev_controller.editDeveloperGet);
+router.post('/:id/edit', authenticateJWT, checkAdmin, dev_controller.editDeveloperPost);
+router.get('/:id/delete', authenticateJWT, checkAdmin, dev_controller.deleteDeveloperGet);
+router.post('/:id/delete', authenticateJWT, checkAdmin, dev_controller.deleteDeveloperPost);
 
 module.exports = router;
 
